d0dc0b90117de60cf224f5b328ccadfc14512efc
mis08 <gantzisideai@gmail.com>
2022-07-22 04:50:32 +0900

    ADD Basic Clock function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
-#include <Arduino.h>
+#include <M5StickCPlus.h>
+#include <WiFi.h>
+#include "time.h"
+
+const char* ssid = "****";
+const char* password = "*****";
+
+const char* ntpServer = "ntp.nict.jp";
+const long gmtoffset_sec = 0;
+const int daylightoffset_sec = 0;
+
+void printLocalTime();
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.printf("\nConnecting to %s", ssid);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    M5.Lcd.print(".");
+  }
+  M5.Lcd.println("\nCONNECTED!");
+  configTime(gmtoffset_sec,daylightoffset_sec,ntpServer);
+  printLocalTime();
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  delay(20);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000);
+  M5.Lcd.setCursor(0,25);
+  printLocalTime();
+}
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    M5.Lcd.println("Failed to obtain time");
+    return;
+  }
+  M5.Lcd.println(&timeinfo, "%A,%B %d \n%Y %H:%M:%S");
 }
